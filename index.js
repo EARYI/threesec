c40657bbb259eac3888a4b5f9d864b75006177b3
renanbazinin <renanbazinin2@gmail.com>
2016-04-16 14:05:17 +0300

    working version

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ var bodyParser = require('body-parser')
 var rimraf = require('rimraf')
 var multiparty = require('multiparty')
 var path = require('path')
+var js = require('jsonfile')
+// var ffmpeg = require('fluent-ffmpeg')
 
 var rootFolder = __dirname + '/videos/'
-var index = 0;
+var counter = js.readFileSync('./counter.json')
 var app = express()
+
+if (!fs.existsSync(rootFolder)){
+    fs.mkdirSync(rootFolder);
+}
 
 app.use(bodyParser.json())
 
 app.post('/upload', function (req, res, next) {
 var form = new multiparty.Form()
 form.parse(req, function(err, fields, files) {
-    index++;
-    var fileName = rootFolder + index
+    counter.index += 1
+    var fileName = rootFolder + counter.index
 
     mkdirp(fileName, function (err) {
       if (err) return res.status(500).send(err)
@@ -24,7 +30,29 @@ form.parse(req, function(err, fields, files) {
         if (err) return res.status(500).send(err)
         fs.writeFile(fileName + '/1.mp4', data, function (err) {
           if (err) return res.status(500).send(err)
-          return res.send(200)
+          // var proc = ffmpeg()
+          // .input(fs.createReadStream(fileName + '/1.mp4'))
+          // .output(fs.createWriteStream(fileName + '/1.mp4'))
+          // .audioCodec('libfaac')
+          // .videoCodec('libx264')
+          // .size('320x200')
+          // .on('start', function() {
+          //   console.log('file has been starting to convert')
+          // })
+          // .on('progress', function(progress) {
+          //   console.log('Processing: ' + progress.percent + '% done')
+          // })
+          // .on('end', function() {
+          //   console.log('file has been converted succesfully')
+          // })
+          // .on('error', function(err) {
+          //   console.log('an error happened: ' + err.message)
+          // })
+          // .run()
+          js.writeFile('./counter.json', counter, function (err) {
+            if (err) return res.status(500).send(err)
+            return res.send(200)
+          })
         })
       })
     })
@@ -33,47 +61,39 @@ form.parse(req, function(err, fields, files) {
 
 app.get('/download', function (req, res, next) {
   fs.readdir(rootFolder, function (err, files) {
+    console.log(files)
     if (err) return res.status(500).send(err)
     if (files.length === 0) {
       return res.status(404).send({
         message: 'No files left in directory'
       })
     } else {
-          var rand = Math.floor(Math.random() * files.length)
-          var videoFolder = path.join(rootFolder, files[rand])
-      while(videoFolder==null){
-         rand = Math.floor(Math.random() * files.length)
-         videoFolder = path.join(rootFolder, files[rand])
-      }
+      var rand = Math.floor(Math.random() * files.length)
+      var videoFolder = path.join(rootFolder, files[rand])
       fs.readdir(videoFolder, function (err, files) {
         if (err) return res.status(500).send(err)
         var file = files[0]
         var number = parseInt(file.split('.')[0], 10)
-        number += 1
         if (number > 3) {
           rimraf(videoFolder, function (err) {
-            if (err) return res.send(500)
+            if (err) return res.status(500).send(err)
             return res.redirect('/download')
           })
         } else {
-          fs.rename(path.join(videoFolder, file), path.join(videoFolder, number + '.mp4'), function (err) {
-            if (err) return res.send(500)
-            fs.readFile(path.join(videoFolder, number + '.mp4'), function (err, data) {
-              if (err) return res.send(500)
+          fs.readFile(path.join(videoFolder, number + '.mp4'), function (err, data) {
+            if (err) return res.status(500).send(err)
+            var newNumber =  number + 1
+            fs.rename(path.join(videoFolder, file), path.join(videoFolder, newNumber + '.mp4'), function (err) {
+              if (err) return res.status(500).send(err)
               return res.send(new Buffer(data, 'binary'))
-            })
-          })  
+            })    
+          })
         }
       })
     }
   })
 })
-
- //app.listen(8080, function(err) {
- //  if (err) throw err
-  // console.log('listening on 8080')
- //})
-
+   
 app.listen(process.env.PORT || 8080, function(err) {
  if (err) throw err
   console.log('listening on 8080')
